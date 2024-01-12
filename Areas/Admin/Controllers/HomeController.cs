13af374525b7620f8b9982e2eeda2735afd34f11
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-01-12 11:29:38 +0300

    Add project files.

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MunicipalityOfRochester.Models;
+using System.Diagnostics;
+
+namespace MunicipalityOfRochester.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
