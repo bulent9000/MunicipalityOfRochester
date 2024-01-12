c44a615bfca3bfba727b130abdb9136f42e9b825
Bülent Karaarslan <137310737+bulent9000@users.noreply.github.com>
2024-01-12 23:56:42 +0300

    DistinctMunicipalityController Changes

---
+++ Areas/Admin/Controllers/DistinctMunicipalityController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Municipality.Data.Repository.IRepository;
+using Municipality.Model;
+
+namespace MunicipalityOfRochester.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class DistinctMunicipalityController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public DistinctMunicipalityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<DistrictMunicipality> DistrictMunicipality = _unitOfWork.DistrictMunicipality.GetAll();
+            return View(DistrictMunicipality);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DistrictMunicipality districtMunicipality)
+        {
+
+            _unitOfWork.DistrictMunicipality.Add(districtMunicipality);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+
+
+    }
+}
